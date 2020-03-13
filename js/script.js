762a071c80defa112a8f4ba7302dc10e87485f1a
Aleksey <59146557+DeadEnd819@users.noreply.github.com>
2020-03-14 01:55:05 +0300

    script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector(`.search-form-btn`);
+var popup = document.querySelector(`.search`);
+
+btn.addEventListener(`click`, function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle(`search-open`);
+})
